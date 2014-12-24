65f4ed745f20fd4ca6473dd947cee61c8e4d695e
johnmetzbower <johnmetzbower@nokware.net>
2014-12-23 22:13:11 -0600

    Master commit

---
+++ Microsoft_Visual_CSharp_Step_by_Step/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Microsoft_Visual_CSharp_Step_by_Step
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
